1af8f5bff20f4c9d6dbe37ad5b72be85ab40807f
Maurice Van Wassenhove <mauricevanwassenhove@gmail.com>
2023-07-14 10:54:24 +0200

    ci: add spotless ktlint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
+    id("com.diffplug.spotless") version "6.19.0"
 }
 
 group = "com.proguard.visualizer"
@@ -26,6 +27,12 @@ kotlin {
             }
         }
         val jvmTest by getting
+    }
+}
+
+spotless {
+    kotlin {
+        ktlint("0.50.0")
     }
 }
 
