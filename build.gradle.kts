ee942915c9cfd64e16e413d72bb9b84eacea271c
Maurice Van Wassenhove <mauricevanwassenhove@gmail.com>
2023-07-14 10:29:52 +0200

    chore: init compose desktop application

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "com.proguard.visualizer"
+version = "1.0.0"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(17)
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "proguard-core-visualizer"
+            packageVersion = "1.0.0"
+        }
+    }
+}
