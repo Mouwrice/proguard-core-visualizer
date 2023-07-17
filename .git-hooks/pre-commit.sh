95ad12257f830ed82c68d915a6bc10c27cd4c5e8
Maurice Van Wassenhove <mauricevanwassenhove@gmail.com>
2023-07-17 15:23:14 +0200

    ci: simplify pre-commit hook

--- .git-hooks/pre-commit.sh
+++ .git-hooks/pre-commit.sh
@@ -2,22 +2,15 @@
 
 echo "Running pre-commit hook..."
 
-if [[ -z "$(git diff --name-only --staged)" ]]; then
-  echo "No files staged. Commit not created."
-  exit 1
-fi
-
 staged_files=$(git diff --name-only --staged "*.kt")
 
-if [[ -z "$staged_files" ]]; then
-  echo "No staged files to format."
-  exit 0
-fi
 
 # Format only the staged files
 ./gradlew spotlessApply
 
-# Add the formatted files to the staging area
-git add "$staged_files"
+# Add the formatted files to the staging area if any
+if [ -n "$staged_files" ]; then
+    git add "$staged_files"
+fi
 
 exit 0
