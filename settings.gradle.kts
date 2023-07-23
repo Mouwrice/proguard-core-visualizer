df91ab6907280804c7cd864e8739d37644a6de4c
Maurice Van Wassenhove <mauricevanwassenhove@gmail.com>
2023-07-23 11:52:57 +0200

    chore: remove comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,13 +16,5 @@ gitHooks {
     commitMsg {
         conventionalCommits()
     }
-    // Ktlint already has a task that creates a `pre-commit` hook:
-    // https://github.com/JLLeitschuh/ktlint-gradle#main-tasks
-    // So we need to manually append to it.
-    // Append
-    // set -e
-    // ./gradlew check
-    // set +e
-    // to .git/hooks/pre-commit.
     createHooks()
 }
