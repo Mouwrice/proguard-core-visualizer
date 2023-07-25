212a46b077955ce1f33e1ace4e3a99e111af90ed
Jitse De Smet <35114273+jitsedesmet@users.noreply.github.com>
2023-07-25 11:42:23 +0200

    feat: use own serializer files (#40)

--- src/jvmMain/kotlin/data/InstructionRecord.kt
+++ src/jvmMain/kotlin/data/InstructionRecord.kt
@@ -12,4 +12,22 @@ data class InstructionRecord(
      * String representation of the instruction
      */
     val instruction: String,
+
+    /**
+     * Contains the final result computations from the partial evaluator regarding the variables of this instruction.
+     */
+    val finalVariablesBefore: List<String>?,
+    /**
+     * Contains the final result computations from the partial evaluator regarding the stack of this instruction.
+     */
+    val finalStackBefore: List<String>?,
+    /**
+     * Contains the final result computations from the partial evaluator regarding the target instructions of this instruction.
+     */
+    val finalTargetInstructions: List<Int>?,
+    /**
+     * Contains the final result computations from the partial evaluator regarding the source instructions of this instruction.
+     */
+    val finalOriginInstructions: List<Int>?,
+
 )
