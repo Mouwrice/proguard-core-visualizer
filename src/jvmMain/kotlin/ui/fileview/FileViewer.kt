51d61afcfa8853288ed6f8a80c93364170ca893c
Maurice Van Wassenhove <mauricevanwassenhove@gmail.com>
2023-07-27 11:34:59 +0200

    style: make it look pwetty

--- src/jvmMain/kotlin/ui/fileview/FileViewer.kt
+++ src/jvmMain/kotlin/ui/fileview/FileViewer.kt
@@ -6,20 +6,24 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import cafe.adriel.bonsai.core.Bonsai
@@ -32,15 +36,15 @@ import ui.codeview.MethodHeader
 import viewmodel.DebuggerViewModel
 
 /**
- * Panel that displays all instructions and a pointer to the current instruction.
- * Also shows the parameters of a method and any errors that occur in the Partial Evaluator.
- * @param viewModel the [DebuggerViewModel] that contains the state of the debugger.
+ * Returns a bonsai tree where the content is the index to the code attribute.
  */
 @Composable
-fun FileViewer(viewModel: DebuggerViewModel?, closeFile: () -> Unit) {
-    val tree = Tree<Pair<String, String?>?> {
-        if (viewModel != null) {
-            Branch(null, {
+fun BonsaiTree(viewModel: DebuggerViewModel, closeFile: () -> Unit): Tree<Int?> {
+    return Tree {
+        Branch(null, customName = {
+            Row {
+                Text(viewModel.file.name, style = MaterialTheme.typography.labelMedium)
+
                 // An IconButton is currently fixed to 48.dp, so we need to make our own.
                 // https://github.com/androidx/androidx/blob/androidx-main/compose/material/material/src/commonMain/kotlin/androidx/compose/material/IconButton.kt
                 Box(
@@ -55,37 +59,60 @@ fun FileViewer(viewModel: DebuggerViewModel?, closeFile: () -> Unit) {
                 ) {
                     Icon(Icons.Default.Close, contentDescription = "Close file")
                 }
-            }, name = viewModel?.file?.name ?: "") {
-                viewModel?.codeAttributes?.groupBy { it.clazz }?.forEach { entry ->
-                    val clazz = entry.key
-                    val methods = entry.value
-
-                    Branch(Pair(clazz, null), name = clazz) {
-                        methods.forEach {
-                            Leaf(Pair(clazz, it.method), name = it.method)
-                        }
+            }
+        }) {
+            val branches = HashMap<String, MutableList<Pair<Int, String>>>()
+            viewModel.codeAttributes.forEachIndexed { index, codeAttribute ->
+                branches.getOrPut(codeAttribute.clazz) { mutableListOf() }.add(Pair(index, codeAttribute.method))
+            }
+            branches.forEach { (clazz, methods) ->
+                Branch(null, customName = { Text(clazz, style = MaterialTheme.typography.labelMedium) }) {
+                    methods.forEach { method ->
+                        Leaf(
+                            method.first,
+                            customName = { Text(method.second, style = MaterialTheme.typography.labelSmall) },
+                        )
                     }
                 }
             }
         }
     }
+}
+
+/**
+ * Panel that displays all instructions and a pointer to the current instruction.
+ * Also shows the parameters of a method and any errors that occur in the Partial Evaluator.
+ * @param viewModel the [DebuggerViewModel] that contains the state of the debugger.
+ */
+@Composable
+fun FileViewer(viewModel: DebuggerViewModel?, closeFile: () -> Unit) {
     Row(
-        Modifier.fillMaxWidth(0.5f).fillMaxHeight().border(
+        Modifier.fillMaxWidth(0.6f).fillMaxHeight().border(
             border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline),
             shape = MaterialTheme.shapes.medium,
         ).clip(MaterialTheme.shapes.medium),
     ) {
-        Bonsai(tree, Modifier.fillMaxWidth(0.3f).border(1.dp, Color.Black).fillMaxHeight(), { node ->
-            tree.toggleExpansion(node)
-            if (node.content?.second != null) {
-                val index = viewModel?.codeAttributes?.withIndex()
-                    ?.find { it.value.clazz == node.content?.first && it.value.method == node.content?.second }
+        viewModel?.let {
+            val tree = BonsaiTree(viewModel, closeFile)
+            Bonsai(
+                tree = tree,
+                style = BonsaiStyle(
+                    nodeCollapsedIconColorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                    nodeExpandedIconColorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                    toggleIconColorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                    nodeSelectedBackgroundColor = MaterialTheme.colorScheme.primaryContainer,
+                    nodePadding = PaddingValues(horizontal = 2.dp, vertical = 1.dp),
+                ),
+                modifier = Modifier.fillMaxWidth(0.4f).fillMaxHeight(),
+                onClick = { node ->
+                    tree.clearSelection()
+                    tree.toggleExpansion(node)
+                    tree.selectNode(node)
+                    node.content?.let { viewModel.selectCodeAttribute(it) }
+                },
+            )
+            Divider(thickness = 1.dp, color = MaterialTheme.colorScheme.outline, modifier = Modifier.fillMaxHeight().width(1.dp))
 
-                if (index != null) viewModel.selectCodeAttribute(index.index)
-            }
-        })
-
-        if (viewModel != null) {
             Column {
                 MethodHeader(viewModel.codeAttributes[viewModel.currentCodeAttribute])
 
